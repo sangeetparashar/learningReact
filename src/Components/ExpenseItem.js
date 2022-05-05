2bc44656019e1eb61c838a19a20c436e63f680f1
Sangeet Parashar <sparasha@uwo.ca>
2022-05-05 00:35:44 -0700

    creating a styling component for common css, learning props.children

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,21 +1,16 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 function ExpenseItem(expense) {
-  const month = expense.date.toLocaleString("en-US", { month: "long" });
-  const day = expense.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = expense.date.getFullYear();
   return (
-    <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+    <Card className="expense-item">
+      <ExpenseDate expenseDate={expense.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{expense.title}</h2>
         <div className="expense-item__price">{expense.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
---
+++ src/Components/Expenses.js
@@ -0,0 +1,27 @@
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+
+function Expenses(prop) {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={prop.item[0].title}
+        amount={prop.item[0].amount}
+        date={prop.item[0].date}
+      />
+      <ExpenseItem
+        title={prop.item[1].title}
+        amount={prop.item[1].amount}
+        date={prop.item[1].date}
+      />
+      <ExpenseItem
+        title={prop.item[2].title}
+        amount={prop.item[2].amount}
+        date={prop.item[2].date}
+      />
+    </Card>
+  );
+}
+
+export default Expenses;
